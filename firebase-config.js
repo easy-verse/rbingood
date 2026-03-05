a89af76beb3ff7e5fb1dd3f8a7c6573269599542
Easylablive <87773856+Easylablive@users.noreply.github.com>
2026-03-05 20:07:59 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
+    authDomain: "REPLACE_WITH_PROJECT_ID.firebaseapp.com",
+    projectId: "REPLACE_WITH_PROJECT_ID",
+    storageBucket: "REPLACE_WITH_PROJECT_ID.firebasestorage.app",
+    messagingSenderId: "REPLACE_WITH_SENDER_ID",
+    appId: "REPLACE_WITH_APP_ID"
+};
